fd37ef567288756c3862394c891a203ce9e653ff
trazll <degleted@gmail.com>
2026-01-13 16:30:42 +0300

    Add abstract modules, interfaces and first module implementations

--- src/Main.kt
+++ src/Main.kt
@@ -1,42 +1,51 @@
 fun main() {
     val manager = ResourceManager()
-    val minerals = OutpostResource(1, "Minerals", 300)
-    val gas = OutpostResource(2, "Gas", 100)
-    manager.add(minerals)
-    manager.add(gas)
+    manager.add(OutpostResource(1, "Minerals", 120))
+    manager.add(OutpostResource(2, "Gas", 40))
+    val generator = EnergyGenerator()
+    val lab = ResearchLab()
+    generator.performAction(manager)
+    lab.performAction(manager)
+    println()
     manager.printAll()
-    val bonus = minerals.copy(amount = minerals.amount + 50)
-    println("Копия минералов с бонусом: $bonus")
-//  val pavel: Human = Human("Pavel")
-    val denis: Person = Person("denis", 18)
-    val maksim: Person = Person("Maksim", 20)
-    denis.hello()
-    maksim.hello()
-}
-
-abstract class Human(val name: String) {
-//    fun hello() {
+//    val manager = ResourceManager()
+//    val minerals = OutpostResource(1, "Minerals", 300)
+//    val gas = OutpostResource(2, "Gas", 100)
+//    manager.add(minerals)
+//    manager.add(gas)
+//    manager.printAll()
+//    val bonus = minerals.copy(amount = minerals.amount + 50)
+//    println("Копия минералов с бонусом: $bonus")
+////  val pavel: Human = Human("Pavel")
+//    val denis: Person = Person("denis", 18)
+//    val maksim: Person = Person("Maksim", 20)
+//    denis.hello()
+//    maksim.hello()
+//}
+//
+//abstract class Human(val name: String) {
+////    fun hello() {
+////        println("My name is $name")
+//    abstract var age: Int
+//    abstract fun hello()
+//    }
+//
+//class Person(name: String, override var age : Int): Human(name) {
+//    override fun hello() {
 //        println("My name is $name")
-    abstract var age: Int
-    abstract fun hello()
-    }
-
-class Person(name: String, override var age : Int): Human(name) {
-    override fun hello() {
-        println("My name is $name")
-    }
-}
-
-abstract class Figure {
-    abstract fun perimeter(): Float
-    abstract fun area(): Float
-}
-class Rectangle(val width: Float, val height: Float) : Figure() {
-    override fun perimeter(): Float {
-        return 2 * (width + height)
-    }
-
-    override fun area(): Float {
-        return width * height
-    }
+//    }
+//}
+//
+//abstract class Figure {
+//    abstract fun perimeter(): Float
+//    abstract fun area(): Float
+//}
+//class Rectangle(val width: Float, val height: Float) : Figure() {
+//    override fun perimeter(): Float {
+//        return 2 * (width + height)
+//    }
+//
+//    override fun area(): Float {
+//        return width * height
+//    }
 }
---
+++ src/ResearchLab.kt
@@ -0,0 +1,13 @@
+class ResearchLab : OutpostModule(
+    "Исследовательская лаборатория"
+) {
+    override fun performAction(manager: ResourceManager) {
+        val minerals = manager.get("Minerals")
+        if (minerals == null || minerals.amount < 30) {
+            println("Недостаточно минералово для исследования")
+            return
+        }
+        minerals.amount -= 30
+        println("Лаборатория проводит исследование (минералы -30)")
+    }
+}
