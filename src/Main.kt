180f086f7413a5dafa06a05c5363f7646a56ed63
trazll <degleted@gmail.com>
2026-01-13 15:33:04 +0300

    Add data class OutpostResource and ResourceManager

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val manager = ResourceManager()
+    val minerals = OutpostResource(1, "Minerals", 300)
+    val gas = OutpostResource(2, "Gas", 100)
+    manager.add(minerals)
+    manager.add(gas)
+    manager.printAll()
+    val bonus = minerals.copy(amount = minerals.amount + 50)
+    println("Копия минералов с бонусом: $bonus")
+}
