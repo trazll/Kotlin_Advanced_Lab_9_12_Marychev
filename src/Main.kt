85c06c69ac7dfcbdc6af37a3a6df60fbd3fc0a9b
trazll <degleted@gmail.com>
2026-01-13 15:51:53 +0300

    Add abstract class

--- src/Main.kt
+++ src/Main.kt
@@ -7,4 +7,36 @@ fun main() {
     manager.printAll()
     val bonus = minerals.copy(amount = minerals.amount + 50)
     println("Копия минералов с бонусом: $bonus")
+//  val pavel: Human = Human("Pavel")
+    val denis: Person = Person("denis", 18)
+    val maksim: Person = Person("Maksim", 20)
+    denis.hello()
+    maksim.hello()
+}
+
+abstract class Human(val name: String) {
+//    fun hello() {
+//        println("My name is $name")
+    abstract var age: Int
+    abstract fun hello()
+    }
+
+class Person(name: String, override var age : Int): Human(name) {
+    override fun hello() {
+        println("My name is $name")
+    }
+}
+
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+class Rectangle(val width: Float, val height: Float) : Figure() {
+    override fun perimeter(): Float {
+        return 2 * (width + height)
+    }
+
+    override fun area(): Float {
+        return width * height
+    }
 }
